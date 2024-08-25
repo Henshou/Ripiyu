78e1123b63d19d2d0c9018008708f998cc4134b5
Henshou <127371116+Henshou@users.noreply.github.com>
2024-08-25 08:46:03 +0700

    refactor: change some elements

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Navbar = () => {
     return (
@@ -9,13 +11,16 @@ const Navbar = () => {
                         <div className="flex items-center justify-between h-16">
                             <div className="flex items-center">
                                 <div className="flex shrink-0">
-                                    <a href="/" className="p-2 text-center text-3xl font-bold text-white">Ripiyu</a>
+                                    <a href="/" className="p-2 text-center text-4xl font-bold text-white font-ptserif">Ripiyu</a>
                                 </div>
                             </div>
                             <div className="md-block">
                                 <div className="ml-4 flex items-center space-x-4">
-                                    <a href="/" className="p-2"><Image src="/images/notif_icon.png" alt="notification" width={30} height={30}></Image></a>
-                                    <a href="/" className="text-white p-2">User</a>
+                                    <a href="/" className="p-2"><NotificationsNoneOutlinedIcon className="text-white" fontSize="large"></NotificationsNoneOutlinedIcon></a>
+                                    <div className="flex flex-row items-center">
+                                        <AccountCircleIcon className="text-white" fontSize="large"></AccountCircleIcon>
+                                    <a href="/" className="text-white p-2 text-2xl">User</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
